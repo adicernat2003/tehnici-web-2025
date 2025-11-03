1bb8293d7e3e5ce2d48eaaa46f177371f0b6b2e5
adicernat <adrianvalentin.cernat@endava.com>
2025-11-03 22:58:38 +0200

    format

--- laborator4/src/main/resources/data.sql
+++ laborator4/src/main/resources/data.sql
@@ -1,5 +1,3 @@
-INSERT INTO
-  people (name, email, age)
-VALUES
-  ('Alice', 'alice@example.com', 30),
-  ('Bob', 'bob@example.com', 27);
+INSERT INTO people (name, email, age)
+VALUES ('Alice', 'alice@example.com', 30),
+       ('Bob', 'bob@example.com', 27);
